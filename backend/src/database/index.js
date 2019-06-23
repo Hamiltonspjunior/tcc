4b6e7b51e113526f249b74d937a539c4a75525f7
Hamiltonspjunior <hamiltonspjunior@gmail.com>
2019-06-23 17:28:49 -0300

    Alteração visual no dashboard

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 // Url do MongoDb Atalas online: mongodb+srv://admin:Ifsp@2019@cluster0-re2z0.mongodb.net/tcc?retryWrites=true&w=majority
 // Localhost: mongodb://localhost/tcc
-mongoose.connect('mongodb+srv://admin:Ifsp@2019@cluster0-re2z0.mongodb.net/tcc?retryWrites=true&w=majority', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost/tcc', { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
