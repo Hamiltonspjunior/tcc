e8cfcc0bbb3198227bb3cf6ad3b5912392c96a3c
Hamiltonspjunior <hamiltonspjunior@gmail.com>
2019-06-25 02:11:56 -0300

    Add rotas para listagem, remocao e relacionar iduser com marcacoes

--- backend/src/controllers/markController.js
+++ backend/src/controllers/markController.js
@@ -1,21 +1,62 @@
 const express = require('express');
+const authMiddleware = require('../middlewares/auth');
 
 const Mark = require('../models/mark');
 
 const router = express.Router();
 
-// Rota de registro de usuário
-router.post('/mark', async(req, res) => {
-    //const { email } = req.body;
+// Utiliza o middleware pq a rota precisa ser autenticada
+router.use(authMiddleware);
 
+// Marcar a hora
+router.post('/', async(req, res) => {
     try {
-        const mark = await Mark.create(req.body);
+        const mark = await Mark.create({...req.body, user: req.userId });
 
-        return res.send({ mark })
+        return res.send({ mark });
 
     } catch (err) {
-        return res.status(400).send({ error: 'Erro!' })
+        return res.status(400).send({ error: 'Error creating new marking' });
     }
 });
 
-module.exports = app => app.use('/', router);
+// Listar todas marcações
+router.get('/', async(req, res) => { 
+    try {
+        const marks = await Mark.find().populate('user');
+
+        return res.send({ marks });
+
+    } catch (err) {
+        return res.status(400).send({ error: 'Error loading markings' });
+    }
+});
+
+// Listar marcação por id
+router.get('/:userId', async(req, res) => { 
+    try {
+        const marks = await Mark.findById(req.params.userId).populate('user');
+
+        return res.send({ marks });
+
+    } catch (err) {
+        return res.status(400).send({ error: 'Error loading markings' });
+    }
+});
+
+router.put('/:userId', async(req, res) => { 
+    return res.send({ user: req.userId });
+});
+
+router.delete('/:userId', async(req, res) => { 
+    try {
+        await Mark.findByIdAndRemove(req.params.userId);
+
+        return res.send({ message: "Deletado" });
+
+    } catch (err) {
+        return res.status(400).send({ error: 'Error deleting markings' });
+    }
+});
+
+module.exports = app => app.use('/mark', router);
